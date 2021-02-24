6b45ca65fa89e1fadb3c454ff837874270f69dba
Jon Tiritilli <jonathantiritilli@gmail.com>
2021-02-24 09:27:42 -0800

    update video and chunk size

--- server.js
+++ server.js
@@ -20,37 +20,34 @@ app.get('/list', (req, res) => {
 });
 
 app.get('/video', (req, res) => {
-    const path = 'assets/sample.mp4';
-    const stat = fs.statSync(path);
-    const filesize = stat.size;
-    const range = req.headers.range;
+  const path = 'assets/sample.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const CHUNK_SIZE = 10 ** 5;
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = Math.min(start + CHUNK_SIZE, fileSize -1);
 
-    if(range){
-        const parts = range.replace(/bytes=/, "").split("-");
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : filesize - 1;
-
-        const chunksize = (end-start) + 1
-
-        const file = fs.createReadStream(path, {start, end});
-        const head = {
-          'Content-Range': `bytes ${start} - ${end} / ${filesize}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': chunksize,
-          'Content-Type': 'video/mp4',
-        };
-    
-        res.writeHead(206, head)
-
-        file.pipe(res)
-    } else {
-        const head = {
-            'Content-Length': filesize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(path).pipe(res);
+    const contentLength = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': contentLength,
+      'Content-Type': 'video/mp4',
     }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
 });
 
 app.listen(PORT, () => {
